764f71412ce76819ed8e2dc08bd0ade079d4d533
sebastian espeset <sebastian.c.espeset@gmail.com>
2021-03-01 23:26:12 -0600

    added guest working

--- api/potlucks/potlucks-model.js
+++ api/potlucks/potlucks-model.js
@@ -2,56 +2,57 @@ const db = require("../data/db-config");
 const { v4: uuidv4 } = require("uuid");
 
 function find() {
-    return db("potlucks");
+  return db("potlucks");
 }
 
 async function findById(id) {
-    const potluck = await db("potlucks as p")
-        .select("p.location", "p.timestamp", "p.potluck_id", "p.user_id")
-        .where("p.potluck_id", id);
+  const potluck = await db("potlucks as p")
+    .select("p.location", "p.timestamp", "p.potluck_id", "p.user_id")
+    .where("p.potluck_id", id);
 
-    const guests = await db("guests")
-        .select("guest_id", "accepted")
-        .where("potluck_id", id);
+  const guests = await db("guests")
+    .select("guest_id", "accepted")
+    .where("potluck_id", id);
 
-    const items = await db("items as i")
-        .join("users as u", "u.user_id", "i.user_id")
-        .select(
-            "i.item_id",
-            "i.user_id",
-            "i.name",
-            "i.potluck_id",
-            "u.username"
-        );
-    return { ...potluck, guests: guests, items: items };
+  const items = await db("items as i")
+    .join("users as u", "u.user_id", "i.user_id")
+    .select("i.item_id", "i.user_id", "i.name", "i.potluck_id", "u.username");
+  return { ...potluck, guests: guests, items: items };
 }
 
 async function add(newPotluck) {
-    const [id] = await db("potlucks").insert(
-        { ...newPotluck, potluck_id: uuidv4() },
-        "potluck_id"
-    );
-    console.log(id);
-    return findById(id);
+  const [id] = await db("potlucks").insert(
+    { ...newPotluck, potluck_id: uuidv4() },
+    "potluck_id"
+  );
+  console.log(id);
+  return findById(id);
 }
 
-function findPotlucksByUserId(id) {
-    return db("potlucks").where({ user_id: id });
+async function findPotlucksByUserId(id) {
+  const potluck = await db("potlucks").where({ user_id: id });
+  return potluck;
 }
-
-async function updateTime(id, changes){
-    //grab the right potluck
-    const potluck = await db("potlucks as p")
+async function addGuest(userId, newGuest, potluckId) {
+    const newGuestId = uuidv4();
+    const [foo] = await db("guests").insert({guest_id:newGuestId,user_id:newGuest, potluck_id:potluckId, accepted:false},"guest_id")
+    return {guest_id:foo};
+}
+    
+async function updateTime(id, changes) {
+  //grab the right potluck
+  const potluck = await db("potlucks as p")
     .select("p.location", "p.timestamp", "p.potluck_id", "p.user_id")
     .where("p.potluck_id", id)
-    .update(changes,"p.location","p.timestamp");
-     console.log("after changes",potluck)
+    .update(changes, "p.location", "p.timestamp");
+  console.log("after changes", potluck);
 }
 
 module.exports = {
-    add,
-    findById,
-    findPotlucksByUserId,
-    find,
-    updateTime
+  add,
+  findById,
+  findPotlucksByUserId,
+  find,
+  updateTime,
+  addGuest,
 };
--- api/potlucks/potlucks-router.js
+++ api/potlucks/potlucks-router.js
@@ -3,61 +3,85 @@ const Potlucks = require("./potlucks-model");
 const restricted = require("../middleware/restricted"); // a middleware for validate the token.
 
 router.get("/", restricted, (req, res) => {
-    Potlucks.find()
-        .then((potlucks) => {
-            res.status(200).json(potlucks);
-        })
-        .catch((err) => res.send(err));
+  Potlucks.find()
+    .then((potlucks) => {
+      res.status(200).json(potlucks);
+    })
+    .catch((err) => res.send(err));
 });
 
 router.post("/", restricted, (req, res) => {
-    const id = req.decodedToken.id;
-    Potlucks.add({ ...req.body, user_id: id })
-        .then((potluck) => {
-            res.status(200).json(potluck);
-        })
-        .catch((error) => {
-            res.status(400).json(error);
-        });
+  const id = req.decodedToken.id;
+  Potlucks.add({ ...req.body, user_id: id })
+    .then((potluck) => {
+      res.status(200).json(potluck);
+    })
+    .catch((error) => {
+      res.status(400).json(error);
+    });
 });
 
 router.get("/:id", restricted, (req, res) => {
-    const id = req.params.id;
-    Potlucks.findById(id)
-        .then((potluck) => {
-            res.status(200).json(potluck);
-        })
-        .catch((error) => {
-            res.status(400).json(error);
-        });
+  const id = req.params.id;
+  Potlucks.findById(id)
+    .then((potluck) => {
+      res.status(200).json(potluck);
+    })
+    .catch((error) => {
+      res.status(400).json(error);
+    });
 });
 
 router.put("/:id", restricted, (req, res) => {
-    //change location and time of potluck
-    const changes = req.body;
-    const id = req.params.id
-    Potlucks.updateTime(id,changes)
-        .then(potluck=>{
-            res.status(200).json(potluck)
-        }).catch(error=>{
-            res.status(400).json(error)
-        })
+  //change location and time of potluck
+  const changes = req.body;
+  const id = req.params.id;
+  Potlucks.updateTime(id, changes)
+    .then((potluck) => {
+      res.status(200).json(potluck);
+    })
+    .catch((error) => {
+      res.status(400).json(error);
+    });
+});
+router.get("/:id/guest", (req, res) => {
+  const id = req.params.id;
+
+  Potlucks.findPotlucksByUserId(id)
+    .then((potluckGuests) => {
+        console.log(potluckGuests)
+      res.status(200).json(potluckGuests);
+    })
+    .catch((error) => {
+      res.status(400).json(error);
+    });
 });
 
 router.post("/:id/guest", restricted, (req, res) => {
-    //adds a guest to attend
+  //adds a guest to attend
+  const id = req.decodedToken.id;
+  const userId = req.body.user_id;
+  const potluckId = req.params.id;
+
+  // push user into guests array
+  Potlucks.addGuest(id,userId,potluckId)
+    .then((potluckGuests)=>{
+        res.status(200).json(potluckGuests)
+    }).catch((error)=>{
+        res.status(400).json(`message:${error}`)
+    })
 });
 
 router.post("/:id/accept", (req, res) => {
-    //guest accepts invitation
+  //guest accepts invitation
 });
 
 router.post("/:id/item", (req, res) => {
-    //adds an item
+  //adds an item
 });
 
 router.put("/:id/item", (req, res) => {
-    //updates an item with guest bringing the item
+  //updates an item
 });
 
 module.exports = router;
